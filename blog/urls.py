8f862ba6de4c102c5f75d2b7e921de86138c7185
Mike Bozee <msbozee@gmail.com>
2017-12-25 20:37:19 +0000

    fixed detail view url

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
     # ex: /blog/
     path('', views.index, name='index'),
-    # ex: /blog/5/
-    path('<int:blog_id>/', views.detail, name='detail'),
+    # the 'name' value as called by the {% url %} template tag
+    path('<int:post_id>/', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
-# Create your views here.
 
 def index(request):
     latest_post_list = Post.objects.order_by('-date')[:15]
@@ -11,4 +10,6 @@ def index(request):
 
 
 def detail(request, post_id):
-    return HttpResponse("You're looking at post %s." % post_id)
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'blog/detail.html', {'post': post})
+
